8d71b1a675f03bc07f6a6274ebf58c3b6d4c0e06
confh <onexite1@gmail.com>
2026-05-17 07:38:46 +0300

    Update tiny_runtime_linux_amd64 binary for enhanced functionality

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal
+
+echo Building Tiny Windows runtime...
+set GOOS=windows
+set GOARCH=amd64
+go build -ldflags "-s -w" -o src\embedded\tiny_runtime_windows_amd64.exe .\src\cmd\tiny_runtime
+if errorlevel 1 exit /b 1
+
+@REM echo Building Tiny Linux runtime...
+@REM set GOOS=linux
+@REM set GOARCH=amd64
+@REM go build -ldflags "-s -w" -o src\embedded\tiny_runtime_linux_amd64 .\src\cmd\tiny_runtime
+@REM if errorlevel 1 exit /b 1
+
+echo Building Tiny compiler...
+set GOOS=windows
+set GOARCH=amd64
+go build -ldflags "-s -w" -o tiny.exe .\src
+if errorlevel 1 exit /b 1
+
+echo Done.
+endlocal
