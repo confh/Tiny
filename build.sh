8c1f555a15224f832b09e7626fefdf4d846b1593
confh <onexite1@gmail.com>
2026-05-25 19:23:00 +0300

    Add build.sh for linux

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Building Tiny Linux runtime..."
+export GOOS=linux
+export GOARCH=amd64
+go build -ldflags "-s -w" -o src/embedded/tiny_runtime_linux_amd64 ./src/cmd/tiny_runtime || exit 1
+
+echo "Building Tiny compiler..."
+export CGO_ENABLED=1
+export GOOS=linux
+export GOARCH=amd64
+go build -ldflags "-s -w" -o tiny_linux ./src || exit 1
+
+echo "Done."
